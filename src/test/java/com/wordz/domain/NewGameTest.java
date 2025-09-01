b70690816524549111536785191d05ce8a7ee3a8
Bram Eldering <bram.eldering@gmail.com>
2025-09-01 16:17:04 +0200

    Added comments to test

--- src/test/java/com/wordz/domain/NewGameTest.java
+++ src/test/java/com/wordz/domain/NewGameTest.java
@@ -31,11 +31,13 @@ public class NewGameTest {
         wordz.newGame(PLAYER);
 
         Game game = getGameInRepository();
+
         assertThat(game.getWord()).isEqualTo("ARISE");
         assertThat(game.getAttemptNumber()).isZero();
         assertThat(game.getPlayer()).isSameAs(PLAYER);
     }
 
+    // Mock randomNumbers.next and wordRepository.fetchWordByNumber
     private void givenWordToSelect(String wordToSelect) {
         int wordNumber = 2;
 
@@ -47,11 +49,24 @@ public class NewGameTest {
                 .thenReturn(wordToSelect);
     }
 
+//  Verify that a Game object was created and saved to the gameRepository mock object,
+//  and then to capture and return that Game object for further assertions.
     private Game getGameInRepository() {
+        // Create an ArgumentCaptor for the Game.class type
+        // Used to capture method arguments for inspection.
+        // This is useful when you want to make assertions about the object that was passed
+        // to a mocked method, especially when you don't have a direct reference to that object.
         var gameArgument = ArgumentCaptor.forClass(Game.class);
+
+//      Assert that the create method of the gameRepository mock was called.
+//      gameArgument.capture(): It tells Mockito to capture the argument that was passed
+//      to the create method. The captured argument is stored within the gameArgument captor.
         verify(gameRepository)
                 .create(gameArgument.capture());
+        // Retrieve the actual Game object that was captured in the previous step.
         var game = gameArgument.getValue();
+        // Return actual Game object
         return game;
     }
+
 }
